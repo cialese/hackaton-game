a082ac80c6f06e936394e050ca5502ee924198b7
cinthyasegura <cinthyaless@gmail.com>
2019-03-30 18:20:59 -0500

    terminando de anexar imagenes

--- src/bd/questions.js
+++ src/bd/questions.js
@@ -1,32 +1,32 @@
 export const questions = [{
-  'gif': '/img/LaFaldita.gif',
+  'gif': 'https://github.com/cinthyasegura/hackaton-game/blob/gh-pages/img/LaFaldita.gif?raw=true',
   'question': '¿Las chicas que visten ropa apretada, provocan el acoso callejero?',
   'option1': 'El acoso callejero no es provocado por las mujeres',
   'option2': 'Solo las que son bonitas provocan el acoso callejero',
   'answer': '1'
  
 }, {
-  'gif': '/img/Cinderella.gif',
+  'gif': 'https://github.com/cinthyasegura/hackaton-game/blob/gh-pages/img/Cinderella.gif?raw=true',
   'question': '¿Las tareas del hogar son responsabilidad de la mujer?',
   'option1': 'Sí, y sonreir hará que la limpieza sea más divertida',
   'option2': 'No, pueden ser realizadas por hombres o mujeres.',
   'answer': '2'
 }, {
-  'gif': '/img/Lloronas.gif',
+  'gif': 'https://github.com/cinthyasegura/hackaton-game/blob/gh-pages/img/Lloronas.gif?raw=true',
   'question': '¿Las mujeres son muy emocionales para ocupar cargos importantes?',
   'option1': 'Lloran por todo, no pueden tomar decisiones.',
   'option2': 'Muchas mujeres dirigen sus propias compañías',
   'answer': '2'
 },
 {
-  'gif': '/img/Madres.gif',
+  'gif': 'https://github.com/cinthyasegura/hackaton-game/blob/gh-pages/img/Madres.gif?raw=true',
   'question': '¿Todas las mujeres sueñan con ser madres?',
   'option1': 'El instinto maternal es de nacimiento.',
   'option2': 'No todas las mujeres quieren tener hijos/as.',
   'answer': '2'
 },
 {
-  'gif': '/img/Debiles.gif',
+  'gif': 'https://github.com/cinthyasegura/hackaton-game/blob/gh-pages/img/Debiles.gif?raw=true',
   'question': '¿Hay deportes muy bruscos para las mujeres? ',
   'option1': 'Hay mujeres que juegan muy bien al fútbol.',
   'option2': 'Las chicas solo pueden jugar voley',
